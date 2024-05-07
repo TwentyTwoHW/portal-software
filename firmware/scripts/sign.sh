01a4a164c4e85ca8c1736efa07349ce901e06533
Alekos Filini <alekos.filini@gmail.com>
2024-05-07 16:27:44 +0200

    Bump firmware and SDK version

--- firmware/scripts/sign.sh
+++ firmware/scripts/sign.sh
@@ -5,5 +5,5 @@ set -xeo pipefail
 BASE_DIR=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 
 objcopy -O binary "$BASE_DIR/../target/thumbv7em-none-eabihf/release/firmware" /dev/shm/fw.bin
-echo -n "0000006600" | xxd -r -p >> /dev/shm/fw.bin
+echo -n "000000C800" | xxd -r -p >> /dev/shm/fw.bin
 hal key schnorr-sign $(cat "$BASE_DIR/../../../FW_SIGNING_KEY") $(cat /dev/shm/fw.bin | sha256sum | awk '{ print $1 }') | xxd -r -p > /dev/shm/fw-signed.bin && cat /dev/shm/fw.bin >> /dev/shm/fw-signed.bin
